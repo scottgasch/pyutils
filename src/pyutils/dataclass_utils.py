bb1322e17ff599a156501bf30102e1ced946ed49
Scott Gasch <scott@gasch.org>
2024-09-21 18:05:23 -0700

    Move dataclass type hint into typez

--- src/pyutils/dataclass_utils.py
+++ src/pyutils/dataclass_utils.py
@@ -6,22 +6,9 @@ friendly wrappers around conversion to/from Dicts."""
 from __future__ import annotations
 
 import dataclasses
-from typing import Any, Dict, Protocol
+from typing import Any, Dict
 
-
-class Dataclass(Protocol):
-    """Dataclass isn't really a first class type and therefore there is no offical
-    type hint for Dataclasses in Python (yet).  If you need one, here's a suitable
-    stand in.  Example usage::
-
-        def f(d: Dataclass) -> Any:
-            pass
-
-        def g(d: Dict[str, Any]) -> Dataclass:
-            pass
-    """
-
-    __dataclass_fields__: Dict
+from pyutils.typez.dataclass import Dataclass
 
 
 def dataclass_from_dict(dataclass: type, d: Dict[str, Any]) -> Dataclass:
